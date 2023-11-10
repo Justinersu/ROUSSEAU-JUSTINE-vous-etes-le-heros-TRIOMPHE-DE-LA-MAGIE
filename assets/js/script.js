1ffba7102ed1ec9140ec933313f4be044c569d79
Justinersu <justineroussoeau@gmail.com>
2023-11-10 17:16:55 -0500

    Ajouts audios et vidéos

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const chapters = {
         titre: 'En route!',
         description: 'Vous et vos compagnons entrez dans une caverne à la recherche d\'aventures.',
         image: './assets/images/debut.jpg',
+        audio: ' ',
         boutons: [
             {titre: 'Aller de l\'avant', destination: 'troll'}, 
             {titre: 'Rebrousser chemin', destination: 'abandon'}
@@ -12,6 +13,7 @@ const chapters = {
         titre: 'Fin de l\'aventure',
         description: 'Vous abandonnez. Aucun récits ne seronts écrits sur vous.',
         image: './assets/images/abandon.jpg',
+        audio: './assets/audio/fail.mp3',
         boutons: [
             {titre: 'Recommencer', destination: 'debut'}
         ]
@@ -20,6 +22,7 @@ const chapters = {
         titre: 'Rencontre inatendue',
         description: 'Un troll de 12 pieds, arme à la main, se dresse devant vous.',
         image: './assets/images/troll.jpg',
+        audio: './assets/audio/cliquer_bouton.mp3',
         boutons: [
             {titre: 'Continuer', destination: 'attaque'}, 
         ]
@@ -27,7 +30,9 @@ const chapters = {
     attaque: {
         titre: 'À vos armes!',
         description: 'Le troll s\'apprête à attaquer.',
+        video: './assets/videos/magie.mp4',
         image: './assets/images/attaque.jpg',
+        audio: './assets/audio/cliquer_bouton.mp3',
         boutons: [
             {titre: 'Rester en retrait alors que vos compagnons prennent les armes', destination: 'mort'}, 
             {titre: 'Préparer son arme', destination: 'sacoche'}
@@ -37,6 +42,7 @@ const chapters = {
         titre: 'Fin de l\'aventure',
         description: 'Vous et vos compagnons mourrez au combat.',
         image: './assets/images/mort.jpg',
+        audio: './assets/audio/fail.mp3',
         boutons: [
             {titre: 'Recommencer', destination: 'debut'}
         ]
@@ -45,6 +51,7 @@ const chapters = {
         titre: 'L\'équipement du sorcier',
         description: 'Vous ouvrez votre sacoche magique.',
         image: './assets/images/sacoche.jpg',
+        audio: './assets/audio/cliquer_bouton.mp3',
         boutons: [
             {titre: 'Continuer', destination: 'armes'}, 
         ]
@@ -53,6 +60,7 @@ const chapters = {
         titre: 'Un choix judicieux',
         description: 'Plusieurs choix d\'armes s\'offrent à vous.',
         image: './assets/images/armes.jpg',
+        audio: './assets/audio/cliquer_bouton.mp3',
         boutons: [
             {titre: 'Choisir le manuscrit ancien', destination: 'parchemin'}, 
             {titre: 'Choisir le bâton magique', destination: 'baton'}
@@ -62,6 +70,7 @@ const chapters = {
         titre: 'Le manuscrit ancien',
         description: 'Le parchemin est vieux et délabré, mais le sort semble puissant.',
         image: './assets/images/parchemin.jpg',
+        audio: './assets/audio/parchemin.mp3',
         boutons: [
             {titre: 'Continuer', destination: 'coup'}, 
         ]
@@ -69,7 +78,9 @@ const chapters = {
     baton:  {
         titre: 'Le bâton magique',
         description: 'L\'arme classique du sorcier, vous ne manquez jamais un sort lorsqu\'il est entre vos mains.',
+        video: './assets/videos/eclairs.mp4',
         image: './assets/images/baton.jpg',
+        audio: './assets/audio/baton.mp3',
         boutons: [
             {titre: 'Continuer', destination: 'coup'}, 
         ]
@@ -78,6 +89,7 @@ const chapters = {
         titre: 'Attaque surprise',
         description: 'Le troll s\'apprête à vous infliger un coup.',
         image: './assets/images/coup.jpg',
+        audio: './assets/audio/cliquer_bouton.mp3',
         boutons: [
             {titre: 'Lancer un sort', destination: 'sort'}, 
             {titre: 'Esquiver', destination: 'seul'}
@@ -87,6 +99,7 @@ const chapters = {
         titre: 'Fin de l\'aventure',
         description: 'Le coup s\'abat sur vos compagnons. Vous ne pouvez pas achever le troll seul et mourrez.',
         image: './assets/images/seul.jpg',
+        audio: './assets/audio/fail.mp3',
         boutons: [
             {titre: 'Recommencer', destination: 'debut'}
         ]
@@ -96,6 +109,7 @@ const chapters = {
         titre: 'Félicitations!',
         description: 'Vous achevez le troll avec succès et trouvez une montagne de trésors au fond de la caverne.',
         image: './assets/images/felicitations.jpg',
+        audio: './assets/audio/victoire.mp3',
         boutons: [
             {titre: 'Recommencer', destination: 'debut'}
         ]
@@ -105,11 +119,13 @@ const chapters = {
         titre: 'Fin de l\'aventure',
         description: 'Le sort du manuscrit n\'arrive pas à contrer l\'arme du troll. Vous mourrez.',
         image: './assets/images/mort.jpg',
+        audio: './assets/audio/fail.mp3',
         boutons: [
             {titre: 'Recommencer', destination: 'debut'}
         ]
     }
 }
+
 
 function goToChapter(chapter) {
     //Enlève les boutons du div .boutons
@@ -120,9 +136,9 @@ function goToChapter(chapter) {
 
     //Change le contenu des textes fictifs avec le contenu des objets
     if (chapters[chapter]) {
+        document.querySelector('audio').src = chapters[chapter].audio;
         document.querySelector('.titre').innerText = chapters[chapter].titre;
         document.querySelector('.description').innerText = chapters[chapter].description;
-        document.querySelector('.image').src = chapters[chapter].image;
         for (let i = 0; i < chapters[chapter].boutons.length; i++) {
             const nouveauBtn = document.createElement('button'); 
             nouveauBtn.textContent = chapters[chapter].boutons[i].titre; 
@@ -133,6 +149,22 @@ function goToChapter(chapter) {
         }
     } else {
         console.log('Mauvaise clé de chapitre');    
+    }
+
+    //Vérifier si vidéo 
+    if (chapters[chapter].video) {
+        const video = document.createElement('video');
+        video.src = chapters[chapter].video;
+        document.querySelector('.media').innerHTML = '';
+        document.querySelector('.media').appendChild(video);
+        video.setAttributeNode(document.createAttribute('loop'));
+        video.play();
+    } else {
+        console.log('Pas de vidéos pour ce chapitre');
+        const image = document.createElement('img');
+        image.src = chapters[chapter].image;
+        document.querySelector('.media').innerHTML = '';
+        document.querySelector('.media').appendChild(image);
     }
 
     //Twist
